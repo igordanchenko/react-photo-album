da8b5ebd43f5fe9dc43f8ca070efeced02ef3748
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-01-05 09:11:05 -0500

    chore: add columns, rows and masonry examples

---
+++ examples/columns-layout/src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <header>
+            <h1>React Photo Album | Columns Layout</h1>
+            <a href="https://github.com/igordanchenko/react-photo-album" rel="noreferrer noopener">
+                GitHub
+            </a>
+            <a href="https://react-photo-album.com/" rel="noreferrer noopener">
+                Docs
+            </a>
+        </header>
+        <main>
+            <App />
+        </main>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
---
+++ examples/rows-layout/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import PhotoAlbum from "react-photo-album";
+import photos from "./photos";
+
+const App = () => <PhotoAlbum photos={photos} layout="rows" />;
+
+export default App;
