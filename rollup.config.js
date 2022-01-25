f2720896d27c7a57e8a0f99fe10fb9d8b755abd6
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-01-24 19:47:36 -0500

    fix: add support for legacy UMD bundle

--- rollup.config.js
+++ rollup.config.js
@@ -9,28 +9,36 @@ import dts from "rollup-plugin-dts";
 import pkg from "./package.json";
 
 const input = "src/index.ts";
-const packageName = "react-photo-album";
+const packageName = pkg.name;
 
 const dist = "dist";
 const cjsDevelopment = `${packageName}.cjs.development.js`;
 const cjsProduction = `${packageName}.cjs.production.min.js`;
+const umdProduction = `${packageName}.umd.production.min.js`;
 
 const formats = [
     { format: "esm", file: `${dist}/${packageName}.esm.js` },
     { format: "cjs", file: `${dist}/${cjsDevelopment}` },
     { format: "cjs", file: `${dist}/${cjsProduction}`, minify: true },
+    {
+        format: "umd",
+        file: `${dist}/${umdProduction}`,
+        globals: { react: "React" },
+        name: "ReactPhotoAlbum",
+        esModule: false,
+        minify: true,
+    },
 ];
 
 const cjsEntry = `'use strict'\n\nif (process.env.NODE_ENV === 'production') {\n\tmodule.exports = require('./${cjsProduction}');\n} else {\n\tmodule.exports = require('./${cjsDevelopment}');\n}\n`;
 
 // noinspection JSCheckFunctionSignatures
 export default formats
-    .map(({ minify, ...format }) => ({
+    .map(({ minify, ...output }) => ({
         input,
         output: [
             {
-                ...format,
-                esModule: true,
+                ...output,
                 sourcemap: true,
                 exports: "named",
             },
@@ -44,16 +52,13 @@ export default formats
                 babelHelpers: "bundled",
                 extensions: [".js", ".jsx", ".ts", ".tsx"],
             }),
-            ...(minify
-                ? [
-                      terser(),
-                      {
-                          name: "cjs-entry",
-                          buildEnd: () => fs.writeFileSync(pkg.main, cjsEntry),
-                      },
-                  ]
-                : []),
-        ],
+            minify && terser(),
+            minify &&
+                output.format === "cjs" && {
+                    name: "cjs-entry",
+                    buildEnd: () => fs.writeFileSync(pkg.main, cjsEntry),
+                },
+        ].filter(Boolean),
     }))
     .concat([
         {
