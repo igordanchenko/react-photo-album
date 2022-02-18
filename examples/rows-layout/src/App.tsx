d711ca82e0ed0bffd56247e6163ed8fee4418144
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-02-18 12:04:03 -0500

    chore: update codesandbox screenshot

--- examples/rows-layout/src/App.tsx
+++ examples/rows-layout/src/App.tsx
@@ -1,6 +1,6 @@
 import PhotoAlbum from "react-photo-album";
 import photos from "./photos";
 
-const App = () => <PhotoAlbum photos={photos} layout="rows" />;
+const App = () => <PhotoAlbum photos={photos} layout="rows" targetRowHeight={100} />;
 
 export default App;
