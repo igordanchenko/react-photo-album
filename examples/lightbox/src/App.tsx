4c9ceef701caab636bde505fd38d8e63576d0852
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-02-26 17:19:40 -0500

    chore: add lightbox example

---
+++ examples/lightbox/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import PhotoAlbum from "react-photo-album";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
+
+import photos from "./photos";
+
+const App = () => {
+    const [open, setOpen] = useState(false);
+    const [index, setIndex] = useState<number>();
+
+    return (
+        <>
+            <PhotoAlbum
+                photos={photos}
+                layout="rows"
+                onClick={(_, photo) => {
+                    setIndex(photos.findIndex((p) => p === photo));
+                    setOpen(true);
+                }}
+            />
+            {open && index !== undefined && (
+                <Lightbox
+                    enableZoom={false}
+                    clickOutsideToClose={false}
+                    mainSrc={photos[index].src}
+                    nextSrc={photos[(index + 1) % photos.length].src}
+                    prevSrc={photos[(index + photos.length - 1) % photos.length].src}
+                    onCloseRequest={() => setOpen(false)}
+                    onMovePrevRequest={() => setIndex((index + photos.length - 1) % photos.length)}
+                    onMoveNextRequest={() => setIndex((index + 1) % photos.length)}
+                />
+            )}
+        </>
+    );
+};
+
+export default App;
---
+++ examples/lightbox/src/index.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { render } from "react-dom";
+
+import App from "./App";
+import "./index.css";
+
+render(
+    <StrictMode>
+        <header>
+            <h1>React Photo Album | Lightbox</h1>
+            <a href="https://github.com/igordanchenko/react-photo-album" target="_blank" rel="noreferrer noopener">
+                GitHub
+            </a>
+            <a href="https://react-photo-album.com/" target="_blank" rel="noreferrer noopener">
+                Docs
+            </a>
+        </header>
+        <main>
+            <App />
+        </main>
+    </StrictMode>,
+    document.getElementById("root")
+);
