bdbce134c15aeff27b0187b09669e2771216cf2b
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-05-25 09:37:42 -0400

    chore: make build cross-platform

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import dts from "rollup-plugin-dts";
 
@@ -14,7 +15,12 @@ export default [
             { format: "esm", file: pkg.module },
             { format: "cjs", file: pkg.main, exports: "named" },
         ],
-        plugins: [external(), resolve(), typescript({ tsconfig: "tsconfig.build.json" })],
+        plugins: [
+            external(),
+            resolve(),
+            commonjs(),
+            typescript({ tsconfigOverride: { compilerOptions: { removeComments: true } } }),
+        ],
     },
     {
         input,
