222d1afbbb53e4b59999ddf1151fbd6cbdc72abd
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-08-12 17:32:12 -0400

    chore: update photos in the lightbox example

--- examples/lightbox/src/photos.ts
+++ examples/lightbox/src/photos.ts
@@ -1,4 +1,4 @@
-const breakpoints = [2400, 1080, 640, 384, 256, 128, 96, 64, 48];
+const breakpoints = [3840, 2400, 1080, 640, 384, 256, 128, 96, 64, 48];
 
 const unsplashLink = (id: string, width: number, height: number) =>
     `https://source.unsplash.com/${id}/${width}x${height}`;
@@ -27,18 +27,23 @@ const unsplashPhotos = [
     { id: "xKhtkhc9HbQ", width: 1080, height: 1440 },
 ];
 
-const photos = unsplashPhotos.map((photo) => ({
-    src: unsplashLink(photo.id, photo.width, photo.height),
-    width: photo.width,
-    height: photo.height,
-    images: breakpoints.map((breakpoint) => {
-        const height = Math.round((photo.height / photo.width) * breakpoint);
-        return {
-            src: unsplashLink(photo.id, breakpoint, height),
-            width: breakpoint,
-            height,
-        };
-    }),
-}));
+const photos = unsplashPhotos.map((photo) => {
+    const width = breakpoints[0];
+    const height = (photo.height / photo.width) * width;
+
+    return {
+        src: unsplashLink(photo.id, width, height),
+        width,
+        height,
+        images: breakpoints.map((breakpoint) => {
+            const height = Math.round((photo.height / photo.width) * breakpoint);
+            return {
+                src: unsplashLink(photo.id, breakpoint, height),
+                width: breakpoint,
+                height,
+            };
+        }),
+    };
+});
 
 export default photos;
