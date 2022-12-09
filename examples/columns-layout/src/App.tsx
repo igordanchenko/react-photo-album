d3722054b93a0f70be69ce194678a68a3b04def1
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2022-12-08 22:30:09 -0500

    chore: bump react-photo-album in examples to 2.0.0-rc.9

--- examples/columns-layout/src/App.tsx
+++ examples/columns-layout/src/App.tsx
@@ -1,16 +1,6 @@
 import PhotoAlbum from "react-photo-album";
 import photos from "./photos";
 
-const App = () => (
-    <PhotoAlbum
-        photos={photos}
-        layout="columns"
-        componentsProps={{ containerProps: { style: { maxWidth: 1200, margin: "0 auto" } } }}
-        defaultContainerWidth={800}
-        columns={4}
-        spacing={10}
-        padding={0}
-    />
-);
+const App = () => <PhotoAlbum layout="columns" photos={photos} />;
 
 export default App;
