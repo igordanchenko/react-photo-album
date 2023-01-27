e8fa1f68c1fc7193d31dd9c671e4b11a80696880
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2023-01-27 14:39:53 -0500

    chore: exclude tests directory from rollup build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -22,7 +22,7 @@ export default [
             external(),
             resolve(),
             typescript({
-                exclude: ["examples/**/*"],
+                include: ["src/**/*"],
                 compilerOptions: { removeComments: true },
             }),
         ],
