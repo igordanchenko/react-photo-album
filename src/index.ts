3de3d3f266e084836f9ff019ffe5e53ddca7ee6e
bitttttten <bitttttten@googlemail.com>
2024-05-17 15:35:43 +0200

    feat: export some hooks

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import PhotoAlbum from "./PhotoAlbum";
+import computeRowsLayout from './layouts/rows'
+import computeColumnsLayout from './layouts/columns'
+import computeMasonryLayout from './layouts/masonry'
 
 export * from "./types";
 
-export { PhotoAlbum };
+export { PhotoAlbum, computeRowsLayout, computeColumnsLayout, computeMasonryLayout};
 export default PhotoAlbum;
