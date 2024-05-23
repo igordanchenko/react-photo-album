0cf9e1efa7c623b68b274dfb18b374462a4b9de6
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2024-05-23 15:54:25 -0400

    chore: streamline package exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import PhotoAlbum from "./PhotoAlbum";
-
 export * from "./types";
+export { default, default as PhotoAlbum } from "./PhotoAlbum";
 
-export { PhotoAlbum };
-export default PhotoAlbum;
-
+// experimental exports (no semver coverage)
 export { default as unstable_computeRowsLayout } from "./layouts/rows";
 export { default as unstable_computeColumnsLayout } from "./layouts/columns";
 export { default as unstable_computeMasonryLayout } from "./layouts/masonry";
