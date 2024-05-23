37f8f15ebe5403f9b3cffd42a59a69ca3b892029
Aaron Harding <bitttttten@googlemail.com>
2024-05-23 21:42:27 +0200

    feat: export layout algorithms as unstable named exports
    * feat: export layout algorithms as unstable named exports
    
    ---------
    
    Co-authored-by: Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export * from "./types";
 
 export { PhotoAlbum };
 export default PhotoAlbum;
+
+export { default as unstable_computeRowsLayout } from "./layouts/rows";
+export { default as unstable_computeColumnsLayout } from "./layouts/columns";
+export { default as unstable_computeMasonryLayout } from "./layouts/masonry";
