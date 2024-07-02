4398aada2a1f5bf2d3f56aaf31e9f5968c14f4e6
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2024-07-01 20:34:00 -0400

    feat: enhance SSR component styling options

--- src/ssr/SSR.tsx
+++ src/ssr/SSR.tsx
@@ -1,9 +1,10 @@
 import type React from "react";
 import { cloneElement, isValidElement, useState } from "react";
 
+import { clsx } from "../core/utils";
 import { useContainerWidth } from "../client/hooks";
+import { StyledBreakpoints, useBreakpoints } from "./breakpoints";
 import { CommonPhotoAlbumProps } from "../types";
-import { StyledBreakpoints, useBreakpoints } from "./breakpoints/index";
 
 /** SSR component props. */
 export type SSRProps = {
@@ -11,10 +12,19 @@ export type SSRProps = {
   breakpoints: number[];
   /** Photo album instance, which must be the only child. */
   children: React.ReactElement<Pick<CommonPhotoAlbumProps, "breakpoints" | "defaultContainerWidth">>;
+  /** If `true`, do not include the inline stylesheet. */
+  unstyled?: boolean;
+  /** Custom class names for the container and the breakpoint intervals. */
+  classNames?: {
+    /** Custom container class name. */
+    container?: string;
+    /** Custom class names for the breakpoint intervals. */
+    breakpoints?: { [key: number]: string };
+  };
 };
 
 /** Experimental SSR component. */
-export default function SSR({ breakpoints: breakpointsProp, children }: SSRProps) {
+export default function SSR({ breakpoints: breakpointsProp, unstyled, classNames, children }: SSRProps) {
   const { breakpoints, containerClass, breakpointClass } = useBreakpoints("ssr", breakpointsProp);
   const { containerRef, containerWidth } = useContainerWidth(breakpoints);
   const [hydratedBreakpoint, setHydratedBreakpoint] = useState<number>();
@@ -27,7 +37,7 @@ export default function SSR({ breakpoints: breakpointsProp, children }: SSRProps
 
   return (
     <>
-      {hydratedBreakpoint === undefined && (
+      {!unstyled && hydratedBreakpoint === undefined && (
         <StyledBreakpoints
           breakpoints={breakpoints}
           containerClass={containerClass}
@@ -35,11 +45,14 @@ export default function SSR({ breakpoints: breakpointsProp, children }: SSRProps
         />
       )}
 
-      <div ref={containerRef} className={containerClass}>
+      <div ref={containerRef} className={clsx(containerClass, classNames?.container)}>
         {breakpoints.map(
           (breakpoint) =>
             (hydratedBreakpoint === undefined || hydratedBreakpoint === breakpoint) && (
-              <div key={breakpoint} className={breakpointClass(breakpoint)}>
+              <div
+                key={breakpoint}
+                className={clsx(breakpointClass(breakpoint), classNames?.breakpoints?.[breakpoint])}
+              >
                 {cloneElement(children, { breakpoints, defaultContainerWidth: breakpoint })}
               </div>
             ),
