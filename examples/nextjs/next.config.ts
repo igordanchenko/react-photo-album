e31d7eb8a9e48ca55eec397638734793183142d6
Igor Danchenko <64441155+igordanchenko@users.noreply.github.com>
2026-02-12 17:40:15 -0800

    chore: update dependencies in examples

--- examples/nextjs/next.config.ts
+++ examples/nextjs/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "images.react-photo-album.com" }],
   },
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
--- examples/server/next.config.ts
+++ examples/server/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: __dirname,
+  },
+};
 
 export default nextConfig;
